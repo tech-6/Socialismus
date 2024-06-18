488136d08a3ba1f7b36a09e4bbad08fcf892bf46
whereareiam <mmm.g032@gmail.com>
2024-06-18 11:59:10 +0200

    chore: update to 1.21

--- socialismus-platform/platform-velocity/src/main/java/me/whereareiam/socialismus/platform/velocity/inject/VelocityInjectorConfiguration.java
+++ socialismus-platform/platform-velocity/src/main/java/me/whereareiam/socialismus/platform/velocity/inject/VelocityInjectorConfiguration.java
@@ -5,9 +5,11 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import me.whereareiam.socialismus.api.input.DependencyResolver;
 import me.whereareiam.socialismus.api.output.LoggingHelper;
 import me.whereareiam.socialismus.api.output.PlatformMessenger;
+import me.whereareiam.socialismus.api.output.Scheduler;
 import me.whereareiam.socialismus.platform.velocity.VelocityDependencyResolver;
 import me.whereareiam.socialismus.platform.velocity.VelocityLoggingHelper;
 import me.whereareiam.socialismus.platform.velocity.VelocityPlatformMessenger;
+import me.whereareiam.socialismus.platform.velocity.VelocityScheduler;
 
 public class VelocityInjectorConfiguration extends AbstractModule {
 	private final ProxyServer proxyServer;
@@ -24,6 +26,7 @@ public class VelocityInjectorConfiguration extends AbstractModule {
 		bind(DependencyResolver.class).toInstance(dependencyResolver);
 
 		bind(LoggingHelper.class).to(VelocityLoggingHelper.class);
+		bind(Scheduler.class).to(VelocityScheduler.class);
 		bind(PlatformMessenger.class).to(VelocityPlatformMessenger.class);
 	}
 }
