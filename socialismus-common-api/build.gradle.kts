304351ea67cb1dfaee0481ded43ee02f0be72096
whereareiam <mmm.g032@gmail.com>
2024-08-03 16:04:25 +0200

    chore: build javadocs.

--- socialismus-common-api/build.gradle.kts
+++ socialismus-common-api/build.gradle.kts
@@ -1,3 +1,23 @@
 dependencies {
     "compileOnly"(libs.bundles.adventure)
 }
+
+tasks.register("generateJavadocs", Javadoc::class) {
+    source = sourceSets["main"].allJava
+    classpath = configurations["compileClasspath"]
+    destinationDir = file("$buildDir/generated/javadoc")
+
+    options {
+        title = "Socialismus API"
+        windowTitle = "Socialismus API"
+    }
+}
+
+tasks.register("javadocJar", Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks["generateJavadocs"])
+}
+
+tasks.named("build") {
+    dependsOn("javadocJar")
+}
